5952bc4f0b9d7dbb9477e761e4e53aaf9e5d2753
IniZio <newman.kcchow@gmail.com>
2018-04-08 17:57:07 +0800

    build(electron): adds electron support (#1)
    * build(electron): dev script for electron
    
    * build(electron): waits for server before electron
    
    * build(electron): adds script for building electron in production

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// const electron = window.require('electron')
+
+// const fs = electron.remote.require('fs');
+// const ipcRenderer  = electron.ipcRenderer;
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

921ec87860bbec8ab8f176f930877179522fe1be
inizio <newman.kcchow@gmail.com>
2018-04-09 11:23:08 +0800

    build(css): adds tailwind css dependency

--- clipped.config.js
+++ clipped.config.js
@@ -1,6 +1,15 @@
 const path = require('path')
+const tailwindcss = require('tailwindcss')
 
 module.exports = async clipped => {
   clipped.config.dist = path.resolve(__dirname, 'build')
+
+  clipped.config.pwa = !(process.env.TARGET === 'electron')
+
   await clipped.use(require('@clipped/preset-webpack4-frontend'))
+
+  // Adds tailwind css
+  clipped.config.webpack['module.rules.css']
+    .set('use.postcss.options.plugins.tailwind', tailwindcss(path.resolve(__dirname, 'tailwind.config.js')))
+    .set('include.tailwind', path.resolve(__dirname, 'node_modules/tailwindcss'))
 }

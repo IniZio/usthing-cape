927e679e33db2bc90d3c761297bb3d6cdfb93daf
inizio <newman.kcchow@gmail.com>
2018-04-19 08:01:39 +0800

    feat(sidebar): uses sidebar instead of navbar

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import {Route, Switch, Redirect, withRouter} from 'react-router-dom'
+import {Route, Switch, Redirect, withRouter, Link} from 'react-router-dom'
 import {Subscribe} from 'unstated'
 import queryString from 'query-string'
 import R from 'ramda'
 
 import './app.css'
-import {Input} from './primitives'
 import AuthContainer from './containers/auth'
 import LoginPage from './components/login'
 
@@ -22,28 +21,35 @@ const App = withRouter(({location}) => (
                   from="/login"
                   to={decodeURIComponent(queryString.parse(location.search).redirect) || '/'}
                 />,
-                <div key="main">
-                  <nav className="navbar">
-                    <Input className="w-48 focus:w-2/5 hover:w-2/5" placeholder="Search..."/>
-                    <ul className="menu-horizontal text-sm font-extrabold ml-auto h-full">
-                      <li className="flex h-full items-center">
-                        <a>FBS</a>
-                      </li>
-                      <li className="flex h-full items-center" data-trigger="click">
+                <Route key="main">
+                  <div>
+                    <div className="sidebar">
+                      <h3 className="sidebar-header flex items-center py-2 my-3 cursor-pointer" data-trigger="click">
                         <img
-                          className="img-avatar max-h-60p w-auto cursor-pointer"
+                          className="img-avatar h-8 w-auto mr-4"
                           src={R.path('profile.image.url'.split('.'), user.state) || `https://api.adorable.io/avatars/71/${user.state.username}@usthing.png`}
                         />
-                      </li>
-                      <div className="dropdown pin-r" style={{top: '100%'}}>
+                        <span>{user.state.profile.username || 'Anonymous'}</span>
+                      </h3>
+                      <div className="dropdown" style={{left: 30, marginTop: -10}}>
                         <ul className="dropdown-menu">
-                          {R.path('profile.username', user.state) && <li className="menn-header">{R.path('profile.username', user.state)}</li>}
                           <li onClick={user.logout}>Logout</li>
                         </ul>
                       </div>
-                    </ul>
-                  </nav>
-                </div>
+                      <div className="h-4"/>
+                      <div className="sidebar-header flex items-center py-1 mt-3">Modules</div>
+                      <Link to="/">
+                        <div className="sidebar-item flex items-center py-2">Home</div>
+                      </Link>
+                      <Link to="/booking">
+                        <div className="sidebar-item flex items-center py-2">FBS</div>
+                      </Link>
+                      <Link to="/marketplace">
+                        <div className="sidebar-item flex items-center py-2">Marketplace</div>
+                      </Link>
+                    </div>
+                  </div>
+                </Route>
               ] :
               [
                 <Route key="login" path="/login" component={LoginPage}/>,
